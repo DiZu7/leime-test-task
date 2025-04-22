f87493f710e0cc74cf9fa8f5fa0056c044d2120b
DiZu7 <dianadovhanyuk@gmail.com>
2025-04-22 16:07:30 +0300

    The project has been initialized

--- src/components/memeCard.tsx
+++ src/components/memeCard.tsx
@@ -10,12 +10,12 @@ type MemeCardProps = {
 
 const MemeCard: React.FC<MemeCardProps> = ({ item }) => (
   <Card key={item.id} className="py-4">
-    <CardHeader className="pb-0 pt-2 px-4 flex justify-between items-start">
+    <CardHeader className="pb-0 pt-0 px-4 flex justify-between items-center">
       <h4 className="font-bold text-large">{item.name}</h4>
       <div className="flex gap-2 items-center">
         <Button
           isIconOnly
-          className="text-default-900/60 -translate-y-2 translate-x-2"
+          className="text-default-900/60"
           radius="full"
           variant="light"
         >
